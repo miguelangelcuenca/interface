a4b3992ea70c1737b16666d81cb74633fb5dd2a6
Miguel Angel Cuenca <miguelangelcuencanavarro86@gmail.com>
2022-04-08 16:28:44 +0200

    update custom token list

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -11,13 +11,11 @@ export const OPTIMISM_LIST = 'https://static.optimism.io/optimism.tokenlist.json
 const ROLL_LIST = 'https://app.tryroll.com/tokens.json'
 const SET_LIST = 'https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json'
 const WRAPPED_LIST = 'wrapped.tokensoft.eth'
+const MY_LIST =
+  'https://raw.githubusercontent.com/miguelangelcuenca/interface/main/src/constants/tokenLists/customList.tokenlist.json'
 
-export const UNSUPPORTED_LIST_URLS: string[] = [BA_LIST]
-
-// this is the default list of lists that are exposed to users
-// lower index == higher priority for token import
-const DEFAULT_LIST_OF_LISTS_TO_DISPLAY: string[] = [
-  UNI_LIST,
+export const UNSUPPORTED_LIST_URLS: string[] = [
+  BA_LIST,
   COMPOUND_LIST,
   AAVE_LIST,
   CMC_ALL_LIST,
@@ -29,7 +27,12 @@ const DEFAULT_LIST_OF_LISTS_TO_DISPLAY: string[] = [
   ROLL_LIST,
   ARBITRUM_LIST,
   OPTIMISM_LIST,
+  UNI_LIST,
 ]
+
+// this is the default list of lists that are exposed to users
+// lower index == higher priority for token import
+const DEFAULT_LIST_OF_LISTS_TO_DISPLAY: string[] = [MY_LIST]
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   ...DEFAULT_LIST_OF_LISTS_TO_DISPLAY,
@@ -37,4 +40,4 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
 ]
 
 // default lists to be 'active' aka searched across
-export const DEFAULT_ACTIVE_LIST_URLS: string[] = [UNI_LIST, GEMINI_LIST]
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = [MY_LIST]
